54797295505c312b2e6150608dea72b1c28183ac
kailash360 <kailashkejriwal21@gmail.com>
2021-06-20 14:12:45 +0530

    Added user-profile page

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
-//Node Server to be created here
-const http = require('http');
-
+const express = require("express")
+const fs = require('fs')
+const path = require("path")
+const bodyparser = require("body-parser")
+const app = express()
 const hostname = '127.0.0.1';
 const port = 80;
+
+//Setting source of static,javascripts,
+app.use('/static', express.static('static'))
+app.use('/scripts', express.static('scripts'))
+app.use(express.urlencoded())
+
+
+//Setting source of html files 
+app.set('view engine', 'html')
+app.set('views', path.join(__dirname, 'views'))
+
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
