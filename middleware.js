1bdd1e3ae6e7f4ab4fb1ced0ac241b136c053411
kailash360 <kailashkejriwal21@gmail.com>
2021-06-27 13:48:23 +0530

    Added middleware for customer-authentication

--- middleware.js
+++ middleware.js
@@ -35,4 +35,30 @@ const seller_auth = async(req, res, next) => {
 
 };
 
-module.exports = seller_auth
+const customer_auth = async(req, res, next) => {
+    try {
+        const token = req.headers.authorization.split(" ")[1];
+        const decodedToken = jwt.verify(token, process.env.JWT_KEY);
+        const username = decodedToken.username;
+        req.locals = { 'customer_username': username }
+        await client
+            .query(`select * from customers where username='${username}'`)
+            .then(response => {
+                if (response.rows.length == 0) {
+                    throw 'Invalid user ID';
+                } else {
+                    next();
+                }
+            })
+    } catch {
+        res.status(401).json({
+            error: new Error('Invalid request!')
+        });
+    }
+
+};
+
+module.exports = {
+    'seller_auth': seller_auth,
+    'customer_auth': customer_auth,
+}
