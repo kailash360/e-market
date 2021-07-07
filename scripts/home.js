bb63580045576a30f4560b16873df4de29d062e9
kailash360 <kailashkejriwal21@gmail.com>
2021-07-08 05:23:22 +0530

    Finished carousel

--- scripts/home.js
+++ scripts/home.js
@@ -2,40 +2,43 @@ console.log("Integrated successfully");
 var slideIndex = 1;
 showSlides(slideIndex);
 
-   var modal = document.getElementById("myModal");
+var modal = document.getElementById("myModal");
 
-    function openPopup(img) {
-      var modalImg = document.getElementById("img01");
-      var captionText = document.getElementById("caption");
-      modal.style.display = "block";
-      modalImg.src = img.src;
-      captionText.innerHTML = img.alt;
-    }
+function openPopup(img) {
+    var modalImg = document.getElementById("img01");
+    var captionText = document.getElementById("caption");
+    modal.style.display = "block";
+    modalImg.src = img.src;
+    captionText.innerHTML = img.alt;
+}
 
-    function closePopup() {
-      modal.style.display = "none";
-    }
+function closePopup() {
+    modal.style.display = "none";
+}
 
 function plusSlides(n) {
-  showSlides(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
 function currentSlide(n) {
-  showSlides(slideIndex = n);
+    showSlides(slideIndex = n);
 }
 
 function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length)
-  {slideIndex = 1}    
-  if (n < 1) 
-  {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) 
-  {
-      slides[i].style.display = "none";  
-  }
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
 
-  slides[slideIndex-1].style.display = "block";  
+    slides[slideIndex - 1].style.display = "block";
 }
+
+document.querySelectorAll(".box").forEach(item => {
+    item.addEventListener("click", () => {
+        window.location.href = "/products-page"
+    })
+})
